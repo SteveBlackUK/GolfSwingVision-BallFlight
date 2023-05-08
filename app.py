d3e6daa5ea74f1dc65f5b96f8c4b96f736c506a1
steve <steve@purplewildcat.com>
2023-05-08 13:40:42 +0100

    working version of mvee calc

--- app.py
+++ app.py
@@ -1,15 +1,31 @@
+import os
 from flask import Flask, jsonify, request
 import json
 from flask_cors import CORS  # Add this import
 from golf_ballistics import golf_ballistics
+from mvee import mvee
+import numpy as np
+from scipy.linalg import eigh
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
-CORS(app)  # Add this line to enable CORS
+flask_env = os.environ.get("FLASK_ENV", "development")
+
+if flask_env == "development":
+    # Allow all origins for local development
+    CORS(app, resources={r"/api/*": {"origins": "*"}})
+else:
+    # Replace 'your-mern-app-domain.com' with your actual MERN app domain
+    CORS(app, resources={
+         r"/api/*": {"origins": "https://golfswingvision.com/"}})
+
 
 @app.route('/calculate-ball-flight', methods=['POST'])
 def processData():
     # Parse the JSON data
-    data = request.json 
+    data = request.json
 
     shot = data
 
@@ -27,13 +43,13 @@ def processData():
     # Convert units
     velocity = velocityMPH * 0.44704
 
-  # Call the initiate_hit method
+    # Call the initiate_hit method
     ball.initiate_hit(velocity, launch_angle_deg, off_center_angle_deg,
-                    backspin_rpm, -sidespin_rpm, windspeed, windheading_deg)
+                      backspin_rpm, -sidespin_rpm, windspeed, windheading_deg)
 
     # Call the get_landingpos method
     x, y = ball.get_landingpos(velocity=velocity, launch_angle_deg=launch_angle_deg, off_center_angle_deg=off_center_angle_deg,
-                    backspin_rpm=backspin_rpm, sidespin_rpm=-sidespin_rpm, windspeed=windspeed, windheading_deg=windheading_deg)
+                               backspin_rpm=backspin_rpm, sidespin_rpm=-sidespin_rpm, windspeed=windspeed, windheading_deg=windheading_deg)
 
     # Convert output units back to yards
     x_yards, y_yards = x * 1.09361, y * 1.09361
@@ -44,18 +60,33 @@ def processData():
     # Convert the maximum z-value from meters to yards
     max_z_yards = max_z * 1.09361
 
-    # Filter the dataframe to keep only rows with non-negative z-values
-    filtered_df_simres = ball.df_simres[ball.df_simres['z'] >= 0]
+    def get_first_negative_z_index(df):
+        for index, row in df.iterrows():
+            if row['z'] < 0:
+                return index
+        return -1
+
+    # Find the index of the first row where z goes below zero
+    first_negative_z_index = get_first_negative_z_index(ball.df_simres)
+
+    # If there's a row with a negative z-value, include it and set its z value to 0
+    if first_negative_z_index != -1:
+        # ball.df_simres.at[first_negative_z_index, 'z'] = 0
+        filtered_df_simres = ball.df_simres.iloc[:first_negative_z_index + 1]
+    else:
+        filtered_df_simres = ball.df_simres
 
     # Convert x, y, and z values from meters to yards
-    filtered_df_simres[['x', 'y', 'z']] = filtered_df_simres[['x', 'y', 'z']] * 1.09361
+    filtered_df_simres[['x', 'y', 'z']
+                       ] = filtered_df_simres[['x', 'y', 'z']] * 1.09361
 
     # Drop the 't', 'v_x', 'v_y', and 'v_z' columns
-    filtered_df_simres = filtered_df_simres.drop(columns=['t', 'v_x', 'v_y', 'v_z'])
+    filtered_df_simres = filtered_df_simres.drop(
+        columns=['t', 'v_x', 'v_y', 'v_z'])
 
     # Convert the dataframe to a JSON object
     df_simres_json = filtered_df_simres.to_json(orient="records")
-    
+
     # Format the result as a JSON object
     result = {
         "landing_position": {"x": x_yards, "y": y_yards},
@@ -65,6 +96,44 @@ def processData():
     }
 
     return jsonify(result)
+
+
+@app.route('/calculate-mvee', methods=['POST'])
+def calculate_mvee():
+    # Parse the JSON data
+    data = request.json
+    points_json = data['points']
+
+    # Convert the list of dictionaries to a numpy array
+    points = np.array([[point[0], point[1]] for point in points_json])
+
+    # Call the mvee function
+    matrix_A, center = mvee(points, tol=1e-4)
+
+    print(matrix_A)
+    print("end of matrix A")
+    # Calculate the eigenvalues and eigenvectors of matrix A
+    eigenvalues, eigenvectors = eigh(matrix_A)
+
+    # Calculate the lengths of the semi-major (a) and semi-minor (b) axes
+    a = 1 / np.sqrt(eigenvalues[0])
+    b = 1 / np.sqrt(eigenvalues[1])
+
+    # Calculate the rotation angle using the eigenvectors
+    angle = np.arctan2(eigenvectors[1, 0], eigenvectors[0, 0])
+
+    # plot_ellipse_and_points(points, center, a, b, angle)
+
+    # Format the result as a JSON object
+    response = {
+        "center": center.tolist(),
+        "a": a,
+        "b": b,
+        "angle": angle,
+    }
+
+    return jsonify(response)
+
 
 if __name__ == "__main__":
     from os import environ
---
+++ mvee.py
@@ -0,0 +1,52 @@
+import numpy as np
+import numpy.linalg as la
+# import matplotlib.pyplot as plt
+# from matplotlib.patches import Ellipse
+# import matplotlib
+# matplotlib.use('Agg')
+
+
+def mvee(points, tol=0.001):
+    """
+    Find the minimum volume ellipse.
+    Return A, c where the equation for the ellipse given in "center form" is
+    (x-c).T * A * (x-c) = 1
+    """
+    points = np.asmatrix(points)
+    N, d = points.shape
+    Q = np.column_stack((points, np.ones(N))).T
+    err = tol+1.0
+    u = np.ones(N)/N
+    while err > tol:
+        # assert u.sum() == 1 # invariant
+        X = Q * np.diag(u) * Q.T
+        M = np.diag(Q.T * la.inv(X) * Q)
+        jdx = np.argmax(M)
+        step_size = (M[jdx]-d-1.0)/((d+1)*(M[jdx]-1.0))
+        new_u = (1-step_size)*u
+        new_u[jdx] += step_size
+        err = la.norm(new_u-u)
+        u = new_u
+    c = u*points
+    A = la.inv(points.T*np.diag(u)*points - c.T*c)/d
+    return np.asarray(A), np.squeeze(np.asarray(c))
+
+
+# def plot_ellipse_and_points(points, center, a, b, angle):
+#     fig, ax = plt.subplots()
+
+#     # Plot the points
+#     ax.scatter(points[:, 0], points[:, 1], color='blue')
+
+#     # Create and plot the ellipse
+#     ellipse = Ellipse(xy=center, width=2*a, height=2*b,
+#                       angle=np.degrees(angle), edgecolor='red', fill=False)
+#     ax.add_artist(ellipse)
+
+#     # Set the aspect ratio to be equal
+#     ax.set_aspect('equal', adjustable='box')
+
+#     plt.savefig('ellipse_plot.png')
+
+#     # Show the plot
+#     plt.show()
